f21dec31eeea55341145dcb3427ad026c1a299d1
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-13 23:52:41 +0600

    1. jwt is not working , 2. /user collection e user data post hocce na

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -12,6 +12,7 @@ import {
 } from 'firebase/auth'
 import { app } from '../firebase/firebase.config'
 import axios from 'axios'
+import useAxiosPublic from '../hooks/useAxiosPublic'
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext(null)
@@ -21,6 +22,7 @@ const googleProvider = new GoogleAuthProvider()
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
+  const axiosPublic = useAxiosPublic();
 
   const createUser = (email, password) => {
     setLoading(true)
@@ -55,18 +57,21 @@ const AuthProvider = ({ children }) => {
       console.log('CurrentUser-->', currentUser?.email)
       if (currentUser?.email) {
         setUser(currentUser)
+        // save user info in db
+        await axios.post(
+          `http://localhost:5000/users/${currentUser?.email}`,
+          {
+            name: currentUser?.displayName,
+            image: currentUser?.photoURL,
+            email: currentUser?.email,
+          }
+        )
 
         // Get JWT token
-        await axios.post(
-          `${import.meta.env.VITE_API_URL}/jwt`,
-          {
-            email: currentUser?.email,
-          },
-          { withCredentials: true }
-        )
+        await axios.post(`http://localhost:5000/jwt`,{ email: currentUser?.email,},{ withCredentials: true })
       } else {
         setUser(currentUser)
-        await axios.get(`${import.meta.env.VITE_API_URL}/logout`, {
+        await axios.get(`http://localhost:5000/logout`, {
           withCredentials: true,
         })
       }
@@ -76,6 +81,33 @@ const AuthProvider = ({ children }) => {
       return unsubscribe()
     }
   }, [])
+
+
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, currentUser => {
+  //     setUser(currentUser)
+  //     // console.log('CurrentUser-->', currentUser)
+  //     if(currentUser){
+  //       const userInfo ={
+  //         email: currentUser.email
+  //       }
+  //       axiosPublic.post('/jwt',userInfo )
+  //       .then(res=>{
+  //         if(res.data.token){
+  //           localStorage.setItem('access-token', res.data.token);
+  //           setLoading(false)
+  //         }
+  //       })
+  //     }else{
+  //       localStorage.removeItem('access-token')
+  //       setLoading(false)
+  //     }
+      
+  //   })
+  //   return () => {
+  //     return unsubscribe()
+  //   }
+  // }, [axiosPublic])
 
   const authInfo = {
     user,
