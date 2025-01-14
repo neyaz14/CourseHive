7c2d9edec6559e96f3762d041ae65e9eb4d141d5
neyaz <neyazmorshid@gmail.com>
2025-01-14 08:34:06 +0600

    some pages added

---
+++ src/pages/TeachOnCourseHive.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeachOnCourseHive = () => {
+    return (
+        <div>
+            <p>It will be a form section </p>
+        </div>
+    );
+};
+
+export default TeachOnCourseHive;
