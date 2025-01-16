e0823936cd6d10fbe627e3f7055eb361ef3bc3b4
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-16 22:02:59 +0600

    teacher request page all done

--- src/pages/Dashboard/Admin/AllUsers.jsx
+++ src/pages/Dashboard/Admin/AllUsers.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import useAlluser from '../../../hooks/useAlluser';
+import LoadingSpinner from '../../../components/Shared/LoadingSpinner';
 
 const AllUsers = () => {
+    const {user} = useAuth();
+    const [allUsers, refetch, isLoading] = useAlluser();
+    if(isLoading) return <LoadingSpinner></LoadingSpinner>
     return (
-        <div>
+        <section>
             
-        </div>
+        </section>
     );
 };
 
--- src/pages/Dashboard/Admin/TeacherRequest.jsx
+++ src/pages/Dashboard/Admin/TeacherRequest.jsx
@@ -2,70 +2,68 @@ import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import useAlluser from '../../../hooks/useAlluser';
 import LoadingSpinner from '../../../components/Shared/LoadingSpinner';
-import { FaUsers  } from 'react-icons/fa';
-// import { ImCancelCircle } from "react-icons/im";
+import { FaUsers } from 'react-icons/fa';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import Swal from 'sweetalert2'
 
 const TeacherRequest = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     const [users, refetch, isLoading] = useAlluser()
     const AxiosSecure = useAxiosSecure();
-    if(isLoading) return <LoadingSpinner></LoadingSpinner>;
-    const RequestedStudents = [...users].filter(teacher=>teacher?.status ==='requested')
-    // console.log(RequestedStudents)
-// make teacher 
-const handleMakeTeacher=async (use)=>{
-    // console.log(use.email)
-    try{
-        const res = await AxiosSecure.patch(`/user/teacher/${use.email}`)
-        console.log(res.data)
-        if(res.data. modifiedCount){
+    if (isLoading) return <LoadingSpinner></LoadingSpinner>;
+    const RequestedStudents = [...users].filter(teacher => teacher?.status === 'requested')
+    // make teacher 
+    const handleMakeTeacher = async (use) => {
+        // console.log(use.email)
+        try {
+            const res = await AxiosSecure.patch(`/user/teacher/${use.email}`)
+            console.log(res.data)
+            if (res.data.modifiedCount) {
+                Swal.fire({
+                    title: "Successfully updated role as a teacher",
+                    icon: "success",
+                    draggable: true
+                });
+            }
+
+        } catch (err) {
+            console.log(err)
             Swal.fire({
-                title: "Successfully updated role as a teacher",
-                icon: "success",
+                title: { err },
+                icon: "error",
                 draggable: true
-              });
+            });
         }
-        
-    }catch(err){
-        console.log(err)
-        Swal.fire({
-            title: {err},
-            icon: "error",
-            draggable: true
-          });
+
     }
 
-}
+    const handleReject = async (useR) => {
+        try {
+            const res = await AxiosSecure.patch(`/user/teacher/reject/${useR.email}`)
+            console.log(res.data)
+            if (res.data.modifiedCount) {
+                Swal.fire({
+                    title: "Successfully rejected ",
+                    icon: "success",
+                    draggable: true
+                });
+            }
 
-const handleReject = async (useR)=>{
-    try{
-        const res = await AxiosSecure.patch(`/user/teacher/reject/${useR.email}`)
-        console.log(res.data)
-        if(res.data. modifiedCount){
+        } catch (err) {
+            console.log(err)
             Swal.fire({
-                title: "Successfully rejected ",
-                icon: "success",
+                title: { err },
+                icon: "error",
                 draggable: true
-              });
+            });
         }
-        
-    }catch(err){
-        console.log(err)
-        Swal.fire({
-            title: {err},
-            icon: "error",
-            draggable: true
-          });
     }
-}
 
     return (
         <section>
-             <div className="flex justify-evenly my-4">
-                <h2 className="text-3xl">All Users</h2>
-                <h2 className="text-3xl">Total Users: {RequestedStudents?.length}</h2>
+            <div className="flex justify-evenly my-4">
+                <h2 className="text-3xl">All Teache Requests</h2>
+                <h2 className="text-3xl"> {RequestedStudents?.length}</h2>
             </div>
             <div className="overflow-x-auto text-xs text-black">
                 <table className="table table-zebra w-full">
@@ -73,7 +71,7 @@ const handleReject = async (useR)=>{
                     <thead >
                         {/* TODO : make it responsive */}
                         <tr className='flex items-center justify-between text-black'>
-                            
+
                             <th>Name</th>
                             <th className='w-8 '>Image</th>
                             <th>Expreience</th>
@@ -85,32 +83,32 @@ const handleReject = async (useR)=>{
                     <tbody >
                         {
                             RequestedStudents.map((user, index) =>
-                             <tr
-                             className='flex items-center justify-between'
-                             key={user._id}>
-                               
-                                <td>{user?.name}</td>
-                                <td><img src={user?.image} alt="" className='w-8'/></td>
-                                <td>{user?.experience}</td>
-                                <td>{user?.category}</td>
-                                <td>{user?.status}</td>
-                             
-                                <td className='flex gap-1 md:gap-3'>
-                                    {  <button
-                                        onClick={() => handleMakeTeacher(user)}
-                                        className="btn btn-sm border-none bg-orange-500">
-                                        <FaUsers className="text-white 
+                                <tr
+                                    className='flex items-center justify-between'
+                                    key={user._id}>
+
+                                    <td>{user?.name}</td>
+                                    <td><img src={user?.image} alt="" className='w-8' /></td>
+                                    <td>{user?.experience}</td>
+                                    <td>{user?.category}</td>
+                                    <td>{user?.status}</td>
+
+                                    <td className='flex gap-1 md:gap-3'>
+                                        {<button
+                                            onClick={() => handleMakeTeacher(user)}
+                                            className="btn btn-sm border-none bg-orange-500">
+                                            <FaUsers className="text-white 
                                         text-2xl"></FaUsers>
-                                    </button>}
-                                    {  <button
-                                        onClick={() => handleReject(user)}
-                                        className="btn btn-sm border-none text-white font-bold bg-orange-500">
+                                        </button>}
+                                        {<button
+                                            onClick={() => handleReject(user)}
+                                            className="btn btn-sm border-none text-white font-bold bg-orange-500">
                                             (X)
-                                        {/* <ImCancelCircle className="text-white  */}
-                                        {/*  text-2xl"></ImCancelCircle> */}
-                                    </button>}
-                                </td>
-                            </tr>)
+                                            {/* <ImCancelCircle className="text-white  */}
+                                            {/*  text-2xl"></ImCancelCircle> */}
+                                        </button>}
+                                    </td>
+                                </tr>)
                         }
 
                     </tbody>
